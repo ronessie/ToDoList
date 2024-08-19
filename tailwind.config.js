fabea94a02a05238c0212c6e68fe2e376213c703
Veronika <sakolinskayaveronika@mail.ru>
2024-08-19 21:07:59 +0300

    Add NextUI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
