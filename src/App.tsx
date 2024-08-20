e1968918b72475ee7a2b0161722d3f0a4922b041
Veronika <sakolinskayaveronika@mail.ru>
2024-08-20 12:37:20 +0300

    Add form on LogIn page, update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
 import {Button} from "@nextui-org/button";
 import {Input} from "@nextui-org/input";
+import {Card, CardBody} from "@nextui-org/react";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <form>
-        <Input
-            variant={"underlined"}
-            type="username"
-            label="Username"
-            defaultValue="name"
-            className="max-w-xs"
-        /><br/>
-        <Input
-            variant={"underlined"}
-            type="email"
-            label="Email"
-            defaultValue="name@gmail.com"
-            className="max-w-xs"
-        /><br/>
-        <Input
-            variant={"underlined"}
-            type="password"
-            label="Password"
-            className="max-w-xs"
-        /><br/>
-        <Button color={"primary"} radius={"sm"} style={{width: 320}} type="submit">Register</Button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Card className="App-card">
+                <CardBody>
+                    <div className="LogIn-items">
+                        <Input
+                            variant={"underlined"}
+                            type="username"
+                            label="Username"
+                            defaultValue="name"
+                            className="LogIn-input"
+                        /><br/>
+                        <Input
+                            variant={"underlined"}
+                            type="email"
+                            label="Email"
+                            defaultValue="name@gmail.com"
+                            className="LogIn-input"
+                        /><br/>
+                        <Input
+                            variant={"underlined"}
+                            type="password"
+                            label="Password"
+                            className="LogIn-input"
+                        /><br/>
+                    </div>
+                    <Button radius={"sm"} className="LogIn-button" variant={"shadow"}>Register</Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
