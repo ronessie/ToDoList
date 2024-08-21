5dde4b9aaad9f5aa2292883675a82f1d265b2547
Veronika <sakolinskayaveronika@mail.ru>
2024-08-21 20:28:43 +0300

    Create log in card

--- src/App.tsx
+++ src/App.tsx
@@ -12,34 +12,55 @@ function App() {
                 <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no"/>
             </head>
             <div className="App">
-                <Card className="App-card">
+                {/*<Card className="SignIn-card">*/}
+                {/*    <CardBody>*/}
+                {/*        <h1 className={"Rainbow-text"}>Registration</h1>*/}
+                {/*        <div className="SignIn-items">*/}
+                {/*            <Input*/}
+                {/*                variant={"underlined"}*/}
+                {/*                type="username"*/}
+                {/*                label="Username"*/}
+                {/*                placeholder="name"*/}
+                {/*                className="SignIn-input"*/}
+                {/*            /><br/>*/}
+                {/*            <Input*/}
+                {/*                variant={"underlined"}*/}
+                {/*                type="email"*/}
+                {/*                label="Email"*/}
+                {/*                placeholder="name@gmail.com"*/}
+                {/*                className="SignIn-input"*/}
+                {/*            /><br/>*/}
+                {/*            <Input*/}
+                {/*                variant={"underlined"}*/}
+                {/*                type="password"*/}
+                {/*                label="Password"*/}
+                {/*                className="SignIn-input"*/}
+                {/*            /><br/>*/}
+                {/*        </div>*/}
+                {/*        <Button radius={"sm"} className="SignIn-button" variant={"shadow"}>Register</Button><br/>*/}
+                {/*        <div className={"SignIn-link"}>Already have an account? <Link>Log in</Link></div>*/}
+                {/*    </CardBody>*/}
+                {/*</Card>*/}
+                <Card className="SignIn-card">
                     <CardBody>
-                        <h1 className={"LogIn-text"}>Registration</h1>
-                        <div className="LogIn-items">
-                            <Input
-                                variant={"underlined"}
-                                type="username"
-                                label="Username"
-                                placeholder="name"
-                                className="LogIn-input"
-                            /><br/>
+                        <h1 className={"Rainbow-text"}>Authorization</h1>
+                        <div className="SignIn-items">
                             <Input
                                 variant={"underlined"}
                                 type="email"
                                 label="Email"
                                 placeholder="name@gmail.com"
-                                className="LogIn-input"
+                                className="SignIn-input"
                             /><br/>
                             <Input
                                 variant={"underlined"}
                                 type="password"
                                 label="Password"
-                                className="LogIn-input"
+                                className="SignIn-input"
                             /><br/>
                         </div>
-                        <Button radius={"sm"} className="LogIn-button" variant={"shadow"}>Register</Button><br/>
-                        {/*<div style={{textAlign: "center"}}>Don't have an account yet? <Link>Register now</Link></div>*/}
-                        <div style={{textAlign: "center"}}>Already have an account? <Link>Sign in</Link></div>
+                        <Button radius={"sm"} className="SignIn-button" variant={"shadow"}>Sign in</Button><br/>
+                        <div className={"SignIn-link"}>Don't have an account yet? <Link>Register now</Link></div>
                     </CardBody>
                 </Card>
             </div>
