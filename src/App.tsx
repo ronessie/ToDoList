c988e73efc5a7cc7bbe3ca41eaf633ce27b6fe25
Veronika <sakolinskayaveronika@mail.ru>
2024-08-29 23:07:41 +0300

    Rename css classes, start created checks of valid data on forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useState} from 'react';
+import React, {Dispatch, ReactElement, SetStateAction, useState} from 'react';
 import {Button} from "@nextui-org/button";
 import {Input} from "@nextui-org/input";
 import {Link} from "@nextui-org/link";
@@ -6,57 +6,50 @@ import {Card, CardBody} from "@nextui-org/react";
 import './App.css';
 
 function SignIn(props: { setVisability: Dispatch<SetStateAction<string>> }) {
-        return (
-            <Card className="SignIn-card">
-                <CardBody>
-                    <h1 className={"Rainbow-text"}>Registration</h1>
-                    <div className="SignIn-items">
-                        <Input
-                            variant={"underlined"}
-                            type="username"
-                            label="Username"
-                            placeholder="name"
-                            className="SignIn-input"
-                            maxLength={28}
-                            title={"Max length 28"}
-                        /><br/>
-                        <Input
-                            variant={"underlined"}
-                            type="email"
-                            label="Email"
-                            placeholder="name@gmail.com"
-                            className="SignIn-input"
-                            maxLength={28}
-                            title={"Max length 28"}
-                        /><br/>
-                        <Input
-                            variant={"underlined"}
-                            type="password"
-                            label="Password"
-                            className="SignIn-input"
-                            maxLength={16}
-                            title={"Max length 16"}
-                        /><br/>
-                    </div>
-                    <Button radius={"sm"} className="SignIn-button" variant={"shadow"}>Register</Button><br/>
-                    <div className={"SignIn-link"}>Already have an account? <Link onClick={() => props.setVisability('logIn')}>Log in</Link></div>
-                </CardBody>
-            </Card>
-        );
-}
+    interface SignInFormProps {
+        onRegister: (username: string, email: string, password: string) => void;
+    }
 
-function LogIn(props: { setVisability: Dispatch<SetStateAction<string>> }) {
+    interface LogInFormProps {
+        onAuth: (email: string, password: string) => void;
+    }
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailLog, setEmailLog] = useState('');
+    const [passwordLog, setPasswordLog] = useState('');
+
+    function checkLogInInputs() {
+
+    }
+
+    function checkSignInInputs() {
+
+    }
+
     return (
-        <Card className="SignIn-card">
+        <Card className="Auth-card">
             <CardBody>
-                <h1 className={"Rainbow-text"}>Authorization</h1>
-                <div className="SignIn-items">
+                <h1 className={"Rainbow-text"}>Registration</h1>
+                <div className="Auth-items">
+                    <Input
+                        value={username}
+                        //onChange={setUsername}
+                        variant={"underlined"}
+                        type="username"
+                        label="Username"
+                        placeholder="name"
+                        className="Auth-input"
+                        maxLength={28}
+                        title={"Max length 28"}
+                    /><br/>
                     <Input
                         variant={"underlined"}
                         type="email"
                         label="Email"
                         placeholder="name@gmail.com"
-                        className="SignIn-input"
+                        className="Auth-input"
                         maxLength={28}
                         title={"Max length 28"}
                     /><br/>
@@ -64,13 +57,46 @@ function LogIn(props: { setVisability: Dispatch<SetStateAction<string>> }) {
                         variant={"underlined"}
                         type="password"
                         label="Password"
-                        className="SignIn-input"
+                        className="Auth-input"
                         maxLength={16}
                         title={"Max length 16"}
                     /><br/>
                 </div>
-                <Button radius={"sm"} className="SignIn-button" variant={"shadow"}>Sign in</Button><br/>
-                <div className={"SignIn-link"}>Don't have an account yet? <Link onClick={() => props.setVisability('signIn')}>Register now</Link></div>
+                <Button radius={"sm"} className="Auth-button" variant={"shadow"}>Register</Button><br/>
+                <div className={"Auth-link"}>Already have an account? <Link
+                    onClick={() => props.setVisability('logIn')}>Log in</Link></div>
+            </CardBody>
+        </Card>
+    );
+}
+
+function LogIn(props: { setVisability: Dispatch<SetStateAction<string>> }) {
+    return (
+        <Card className="Auth-card">
+            <CardBody>
+                <h1 className={"Rainbow-text"}>Authorization</h1>
+                <div className="Auth-items">
+                    <Input
+                        variant={"underlined"}
+                        type="email"
+                        label="Email"
+                        placeholder="name@gmail.com"
+                        className="Auth-input"
+                        maxLength={28}
+                        title={"Max length 28"}
+                    /><br/>
+                    <Input
+                        variant={"underlined"}
+                        type="password"
+                        label="Password"
+                        className="Auth-input"
+                        maxLength={16}
+                        title={"Max length 16"}
+                    /><br/>
+                </div>
+                <Button radius={"sm"} className="Auth-button" variant={"shadow"}>Sign in</Button><br/>
+                <div className={"Auth-link"}>Don't have an account yet? <Link
+                    onClick={() => props.setVisability('signIn')}>Register now</Link></div>
             </CardBody>
         </Card>
     );
